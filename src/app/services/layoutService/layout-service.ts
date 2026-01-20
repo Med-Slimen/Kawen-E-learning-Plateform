1f3e2ae74c696b199865ed9aadaa3a41a6551d97
Med-Slimen <mohamedamineslimene01@gmail.com>
2026-01-20 17:08:37 +0100

    back button

--- src/app/services/layoutService/layout-service.ts
+++ src/app/services/layoutService/layout-service.ts
@@ -1,11 +1,12 @@
 import { ViewportScroller } from '@angular/common';
 import { Injectable } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LayoutService {
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor(private viewportScroller: ViewportScroller,private router :Router) {}
 
   scrollTo(anchor: string): void {
     this.viewportScroller.scrollToAnchor(anchor);
